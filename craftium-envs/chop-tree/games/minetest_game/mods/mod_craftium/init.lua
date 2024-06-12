4beb8caacab12c03ee45f2906ad7dc78ad44b436
Mikel <mikelma@tutanota.com>
2024-06-12 15:00:22 +0200

    Hide HUD elements and set timeofday to midday

--- craftium-envs/chop-tree/games/minetest_game/mods/mod_craftium/init.lua
+++ craftium-envs/chop-tree/games/minetest_game/mods/mod_craftium/init.lua
@@ -1,4 +1,4 @@
--- Positive reward if a dirt block is dug
+-- Positive reward if a tree block is dug
 minetest.register_on_dignode(function(pos, node)
   if string.find(node["name"], "tree") then
      -- Give a 1.0 reward once and then reset reward to 0.0 value
@@ -9,4 +9,16 @@ end)
 -- Turn on the termination flag if the agent dies
 minetest.register_on_dieplayer(function(ObjectRef, reason)
       set_termination()
+end)
+
+-- Executed when the player joins the game
+minetest.register_on_joinplayer(function(player, _last_login)
+      -- set timeofday to midday
+      minetest.set_timeofday(0.5)
+
+      -- Disable HUD elements
+      player:hud_set_flags({
+            hotbar = false,
+            crosshair = false,
+      })
 end)
