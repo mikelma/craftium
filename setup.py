9fd105e381c7f790b4eecc002390e859ba39166a
Mikel <mikelma@tutanota.com>
2024-06-17 14:50:38 +0200

    Fix setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,49 @@
 import os
 import shutil
-from skbuild import setup
-from setuptools import find_packages
+from setuptools import setup, Command
+from setuptools.command.build_ext import build_ext as _build_ext
+import subprocess
 
-# Default to using all available CPU cores except two
-num_cores = os.getenv("BUILD_THREADS", max(os.cpu_count()-2, 2))
-os.environ["SKBUILD_BUILD_OPTIONS"] = f"-j{num_cores}"  # will call `make` with `-j`
+
+class BuildCMake(Command):
+    description = "run cmake and make to build the minetest game engine (C++)"
+    user_options = []
+
+    # def initialize_options(self):
+    #     pass
+    #
+    # def finalize_options(self):
+    #     pass
+
+    def run(self):
+        # run cmake
+        subprocess.check_call(["cmake", ".", "-DRUN_IN_PLACE=TRUE", "-DCMAKE_BUILD_TYPE=Release"])
+
+        # default to using all available CPU cores except two
+        num_cores = os.getenv("BUILD_THREADS", max(os.cpu_count()-2, 2))
+
+        # run make
+        subprocess.check_call(["make", f"-j{num_cores}"])
+
+
+class CustomBuildExt(_build_ext):
+    def run(self):
+        self.run_command("build_cpp")
+        _build_ext.run(self)
+
+
+def create_data_dir():
+    data_dir = "craftium/"
+
+    include = ["bin", "builtin", "fonts", "locale",
+               "textures", "client", "craftium-envs"]
+
+    # with open("MANIFEST.in", "w") as manifest:
+    for path in include:
+        tgt = os.path.join(data_dir, path)
+            # manifest.write(f"recursive-include {tgt} *\n")
+        if not os.path.exists(tgt):
+            shutil.copytree(path, tgt)
 
 def setup_irr_shaders_dir():
     # remove the original link
@@ -20,20 +58,11 @@ def setup_irr_shaders_dir():
 
 setup_irr_shaders_dir()
 
+create_data_dir()
+
 setup(
-    name="craftium",
-    version="0.1.0",
-    description="A Gymnasium wrapper for the Minetest voxel game engine",
-    author="Mikel Malagón",
-    author_email="mikelma7@gmail.com",
-    packages=find_packages(where="craftium"),
-    package_dir={"": "craftium"},
-    cmake_args=[
-        "-DRUN_IN_PLACE=TRUE",
-        "-DCMAKE_BUILD_TYPE=Release",
-    ],
-    python_requires=">=3.9",
-    options={
-        "bdist_egg": {"unpack_source": True}
-    }
+    cmdclass={
+        "build_cpp": BuildCMake,
+        "build_ext": CustomBuildExt,
+    },
 )
