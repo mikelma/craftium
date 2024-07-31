4f4a5d02377a0d869de8499ca9ce0ed75dcf8612
Mikel <mikelma@tutanota.com>
2024-07-31 15:44:06 +0200

    train_agent.py: Use gray scale frames and framestack

--- train_agent.py
+++ train_agent.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 import os
 from stable_baselines3 import A2C, PPO
 from stable_baselines3.common import logger
-from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor, VecFrameStack
 import gymnasium as gym
 import craftium
 
@@ -18,9 +18,9 @@ def parse_args():
 
     parser.add_argument("--env-id", type=str, default="Craftium/ChopTree-v0",
         help="Name (registered) of the environment.")
-    parser.add_argument("--total-timesteps", type=int, default=100_000,
+    parser.add_argument("--total-timesteps", type=int, default=10_000_000,
         help="Number of timesteps to train for.")
-    parser.add_argument("--num-envs", type=int, default=1,
+    parser.add_argument("--num-envs", type=int, default=4,
         help="Number of environments to use.")
     parser.add_argument("--method", type=str, default="a2c", choices=["ppo", "a2c"],
         help="RL method to use to optimize the agent.")
@@ -28,6 +28,20 @@ def parse_args():
 
     return parser.parse_args()
 
+def make_env(env_id):
+    def _init():
+        # set up the environment
+        craftium_kwargs = dict(
+            frameskip=3,
+            rgb_observations=False,
+            gray_scale_keepdim=True,
+        )
+
+        env = gym.make(env_id, **craftium_kwargs)
+        env.reset()
+
+        return env
+    return _init
 
 if __name__ == "__main__":
     args = parse_args()
@@ -42,8 +56,8 @@ if __name__ == "__main__":
     print(f"** Storing run's data in {log_path}")
     new_logger = logger.configure(log_path, ["stdout", "csv"])
 
-    # set up the environment
-    envs = DummyVecEnv([lambda: gym.make(args.env_id) for _ in range(args.num_envs)])
+    envs = DummyVecEnv([make_env(args.env_id) for _ in range(args.num_envs)])
+    envs = VecFrameStack(envs, 3)
     envs = VecMonitor(envs)
 
     method_class = PPO if args.method == "ppo" else A2C
