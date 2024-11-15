662259aed91fb9ec7a2db87530aad86274ac5cae
Mikel <mikelma@tutanota.com>
2024-11-15 15:22:15 +0100

    Add mutli-agent-combat environment

---
+++ craftium-envs/multi-agent-combat/clientmods/craftium_mod/init.lua
@@ -0,0 +1,55 @@
+channel_name = "craftium_channel"
+mod_channel = nil
+
+minetest.register_globalstep(function(dtime)
+      -- If soft-reset is requested by craftium, thoen send a message to
+      -- the server to reset the environment
+      if get_soft_reset() == 1 then
+         mod_channel:send_all(
+            minetest.serialize({
+                  agent = "server",
+                  reset = true,
+            })
+         )
+         reset_termination()
+      end
+end)
+
+-- Function to check if the client has fully loaded
+wait_for_client_ready = function()
+    if minetest.localplayer then
+        -- Client has joined the server and is fully loaded
+        callback()
+    else
+        -- Retry after a short delay if not yet fully initialized
+        minetest.after(0.01, function() wait_for_client_ready() end)
+    end
+end
+
+-- Callback executed once the client is fully initialized
+callback = function()
+    -- Join the mod channel and set up listeners here
+    mod_channel = minetest.mod_channel_join(channel_name)
+
+    minetest.register_on_modchannel_message(function(channel, sender, str_message)
+          if channel ~= channel_name then
+             return
+          end
+
+          local msg = minetest.deserialize(str_message)
+
+          if msg.agent ~= minetest.localplayer:get_name() then
+             return
+          end
+
+          if msg.reward ~= nil then
+             set_reward_once(msg.reward, 0.0)
+          end
+
+          if msg.termination == true then
+             set_termination()
+          end
+    end)
+end
+
+wait_for_client_ready()
---
+++ craftium-envs/multi-agent-combat/worlds/world/world.mt
@@ -0,0 +1,14 @@
+enable_damage = false
+creative_mode = true
+mod_storage_backend = sqlite3
+auth_backend = sqlite3
+player_backend = sqlite3
+backend = sqlite3
+gameid = minetest
+world_name = world
+server_announce = false
+load_mod_superflat = mods/superflat
+load_mod_craftium_env = mods/craftium_env
+load_mod_mobs_monster = mods/mobs_monster
+load_mod_mobs = mods/mobs
+load_mod_mobs_animal = false
