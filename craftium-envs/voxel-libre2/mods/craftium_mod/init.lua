7df4bc9c04357e6c8a4fd5428c51349f224dbf45
Eneko Basauri <ebasauri002@ikasle.ehu.eus>
2025-04-10 13:58:59 +0200

    updated OpenWorld environment mod adding record of some info

--- craftium-envs/voxel-libre2/mods/craftium_mod/init.lua
+++ craftium-envs/voxel-libre2/mods/craftium_mod/init.lua
@@ -2,7 +2,7 @@
 init_tools = { "mcl_tools:axe_stone", "mcl_torches:torch 256" }
 
 timeofday_step = 1 / (10000 * minetest.settings:get("frameskip")) -- day/night cycle lasts 10K steps
-timeofday = 0.5 -- start episode at midday
+timeofday = 0 -- start episode at midday
 
 -- executed when the player joins the game
 minetest.register_on_joinplayer(function(player, _last_login)
@@ -23,6 +23,14 @@ minetest.register_on_joinplayer(function(player, _last_login)
 
 	-- set player's initial position
 	player:set_pos({ x = 120, z = 92, y = 16.5 })
+
+	set_info("damage_taken",0.0)
+	set_info("damage_dealt",0.0)
+	set_info("healed_amount",0.0)
+
+	set_empty_dict("mined_nodes")
+	set_empty_dict("defeated_enemies")
+	set_empty_dict("hunted_animals")
 end)
 
 -- turn on the termination flag if the agent dies
@@ -37,6 +45,33 @@ minetest.register_globalstep(function(dtime)
 	end
 	minetest.set_timeofday(timeofday)
 	timeofday = timeofday + timeofday_step
+
+	local player = core.get_connected_players()[1]
+
+	-- if the player is not connected end here
+	if player == nil then
+		return nil
+	end
+
+	-- if the player is connected:
+	-- get the position of the player
+	local player_pos = player:get_pos()
+	set_info("y",player_pos.y)
+	set_info("z",player_pos.z)
+	set_info("x",player_pos.x)
+	-- get the health of the player
+	set_info("health",player:get_hp())
+	-- get stage
+	set_info("stage",curr_stage)
+	-- get inventory
+	set_empty_dict("inventory")
+	local inv = player:get_inventory()
+	local list = inv:get_list("main")  -- "main" is the default inventory list
+	for i, itemstack in ipairs(list) do
+		if not itemstack:is_empty() then
+			add_to_dict("inventory",itemstack:get_name(),itemstack:get_count())
+		end
+	end
 end)
 
 --
@@ -87,6 +122,13 @@ stages = {
 curr_stage = 1 -- index of the current stage
 
 minetest.register_on_dignode(function(pos, node)
+	-- update table of mined nodes of info
+	if dict_contains("mined_nodes", node["name"]) then
+		add_to_dict("mined_nodes",node["name"],get_from_dict("mined_nodes",node["name"])+1)
+	else
+		add_to_dict("mined_nodes",node["name"],1)
+	end
+
 	-- table of the next stage
 	local snext = stages[curr_stage + 1]
 
@@ -135,6 +177,11 @@ for name, _ in pairs(mcl_mobs.spawning_mobs) do -- for all mobs that spawn
 	local old_on_punch = mob_def.on_punch
 	mob_def.on_punch = function(self, puncher, time_from_last_punch, tool_capabilities, dir)
 		local damage = tool_capabilities.damage_groups and tool_capabilities.damage_groups.fleshy or 1
+		if puncher then
+			self.last_puncher_is_player = puncher:is_player()
+		end
+		-- update damage dealt in info
+		set_info("damage_dealt",get_from_info("damage_dealt")+damage)
 		-- provide a different reward for each type of punched mob
 		if mob_type == "monster" then
 			set_reward_once(damage, 0.0)
@@ -148,4 +195,39 @@ for name, _ in pairs(mcl_mobs.spawning_mobs) do -- for all mobs that spawn
 			old_on_punch(self, puncher, time_from_last_punch, tool_capabilities, dir)
 		end
 	end
+
+	-- update the table of defeated enemies of info
+	local old_on_die = mob_def.on_die
+	mob_def.on_die = function(self, killer)
+		if self.last_puncher_is_player then
+			if mob_type == "monster" then
+				if dict_contains("defeated_enemies", name) then
+					add_to_dict("defeated_enemies",name,get_from_dict("defeated_enemies",name)+1)
+				else
+					add_to_dict("defeated_enemies",name,1)
+				end
+			elseif mob_type == "animal" then
+				if dict_contains("hunted_animals", name) then
+					add_to_dict("hunted_animals",name,get_from_dict("hunted_animals",name)+1)
+				else
+					add_to_dict("hunted_animals",name,1)
+				end
+			end
+		end
+
+		-- Call the original on_die function
+		if old_on_die ~= nil then
+			old_on_die(self, killer)
+		end
+	end
 end
+
+minetest.register_on_player_hpchange(function(player, hp_change, reason)
+    if hp_change < 0 then
+        set_info("damage_taken",get_from_info("damage_taken")-hp_change)
+    end
+	if hp_change > 0 then
+        set_info("healed_amount",get_from_info("healed_amount")+hp_change)
+    end
+    return hp_change
+end, true)
