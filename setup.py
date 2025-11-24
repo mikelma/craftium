b8c373de281472d7fad4394aac30cc58b7e4210d
Mikel <mikelma@tutanota.com>
2025-11-24 18:20:16 +0100

    minimal standalone craftium pkg build

--- setup.py
+++ setup.py
@@ -3,28 +3,24 @@ import shutil
 import subprocess
 from setuptools import setup, Extension
 import numpy
+from setuptools.command.build_ext import build_ext
+
+
+STANDALONE_BIN_DIR = "./standalone_luanti"
 
 def build_minetest():
-    subprocess.check_call(["cmake", ".", "-DRUN_IN_PLACE=TRUE", "-DCMAKE_BUILD_TYPE=Release"])
+    if not os.path.exists(STANDALONE_BIN_DIR):
+        subprocess.check_call(["cmake", ".", "-DRUN_IN_PLACE=TRUE", "-DCMAKE_BUILD_TYPE=Release"])
 
-    # default to using all available CPU cores except two
-    num_cores = os.getenv("BUILD_THREADS", max(os.cpu_count()-2, 2))
+        # default to using all available CPU cores except two
+        num_cores = os.getenv("BUILD_THREADS", max(os.cpu_count()-2, 2))
 
-    # run make
-    subprocess.check_call(["make", f"-j{num_cores}"])
+        print("Building Luanti (may take few minutes)...")
+        subprocess.check_call(["make", f"-j{num_cores}"])
 
-def create_data_dir():
-    data_dir = "craftium/"
+        print("Patching Luanti and copying system libraries...")
+        subprocess.check_call(["bash", "build_standalone.sh", STANDALONE_BIN_DIR])
 
-    include = ["bin", "builtin", "fonts", "locale",
-               "textures", "client", "craftium-envs"]
-
-    with open("MANIFEST.in", "w") as manifest:
-        for path in include:
-            tgt = os.path.join(data_dir, path)
-            manifest.write(f"recursive-include {tgt} *\n")
-            if not os.path.exists(tgt):
-                shutil.copytree(path, tgt)
 
 def setup_irr_shaders_dir():
     # remove the original link
@@ -38,14 +34,44 @@ def setup_irr_shaders_dir():
         shutil.copytree(src, path)
 
 
-build_minetest()
+def create_data_dir():
+    data_dir = "craftium-pkg/"
 
-setup_irr_shaders_dir()
+    os.makedirs(data_dir, exist_ok=True)
 
-create_data_dir()
+    include = ["builtin", "fonts", "locale", "irr",
+               "textures", "client", "craftium-envs", "craftium"]
+
+    with open("MANIFEST.in", "w") as manifest:
+        for path in include:
+            tgt = os.path.join(data_dir, path)
+            manifest.write(f"recursive-include {tgt} *\n")
+            if not os.path.exists(tgt):
+                shutil.copytree(path, tgt)
+
+        # copy patched Luanti bin and system libs
+        for dir in ["bin", "lib"]:
+            tgt = os.path.join(data_dir, dir)
+            shutil.copytree(
+                os.path.join(STANDALONE_BIN_DIR, dir),
+                tgt,
+                dirs_exist_ok=True
+            )
+            manifest.write(f"recursive-include {tgt} *\n")
+
+
+class BuildMinetest(build_ext):
+    def run(self):
+        build_minetest()
+        setup_irr_shaders_dir()
+        create_data_dir()
+        super().run()
 
 module = Extension("mt_server",
                    sources=["mt_server.c"],
                    include_dirs=[numpy.get_include()])
 
-setup(ext_modules=[module])
+setup(
+    ext_modules=[module],
+    cmdclass={"build_ext": BuildMinetest},
+)
