da75c9b7f9c429c5f0b96e25ae29ed1f74b908f4
Mikel <mikelma@tutanota.com>
2025-11-28 13:25:03 +0100

    fat wheel build working

--- build_craftium.sh
+++ build_craftium.sh
@@ -4,7 +4,11 @@ set -e
 
 sed -i '/$<$<BOOL:${USE_SDL2_SHARED}>:SDL2::SDL2>/,/$<$<BOOL:${USE_SDL2_STATIC}>:SDL2::SDL2-static>/c\ \ \ \ \ \ \ \ -L/usr/lib64 -lSDL2' irr/src/CMakeLists.txt
 
-cmake . -DRUN_IN_PLACE=TRUE -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSDL2_LIBRARIES="/usr/lib64/libSDL2.so" -DUSE_SDL2_SHARED=TRUE
+cmake . -DRUN_IN_PLACE=TRUE \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DSDL2_LIBRARIES="/usr/lib64/libSDL2.so" \
+        -DUSE_SDL2_SHARED=TRUE
+
 make -j$(nproc)
 
 mkdir -p craftium/luanti
